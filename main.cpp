8edc74786a4cf8be43981106796ae4420d3c83df
WSAL Evan <evan@millers6.com>
2025-10-03 22:39:56 -0400

    GLM Test with core module

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
-#include <iostream>
+#include <glm/glm.hpp>
 #include <core.h>
+#include <iostream>
 
 int main() 
 {
-    WillowVox::Logger::Log("Hello, WillowVox Engine!");
+    glm::vec2 a(1, 2);
+    WillowVox::Logger::Log("Hello, WillowVox Engine! %f %f", a.x, a.y);
 }
