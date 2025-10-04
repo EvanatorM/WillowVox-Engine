04f794a12409485f0775d9252df56d229d08cebf
WSAL Evan <evan@millers6.com>
2025-10-04 16:24:08 -0400

    App integration

--- main.cpp
+++ main.cpp
@@ -1,49 +1,25 @@
-#include <glm/glm.hpp>
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-#include <core.h>
-#include <iostream>
+#include <wv/core.h>
+#include <wv/app/EntryPoint.h>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+using namespace WillowVox;
 
-int main() 
+namespace TestProject
 {
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
-    if (window == NULL)
+    class TestApp : public App
     {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
+        void Start() override
+        {
+            Logger::Log("Start");
+        }
 
-    glfwMakeContextCurrent(window);
-
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
-  
-    glViewport(0, 0, 800, 600);
-
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-    while(!glfwWindowShouldClose(window))
-    {
-        glfwSwapBuffers(window);
-        glfwPollEvents();    
-    }
-
-    glfwTerminate();
-    return 0;
+        void Update() override
+        {
+            Logger::Log("Update");
+        }
+    };
 }
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+WillowVox::App* WillowVox::CreateApp()
 {
-    glViewport(0, 0, width, height);
+    return new TestProject::TestApp();
 }
